a9bfb2c08554af8337562b86e8c07c03d9b9e1dd
Marco Hemken <mehemken@gmail.com>
2018-03-17 07:04:29 -0700

    reorganized the file structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'queruntine',
+    version = '0.1',
+    description = 'Python asks Rust to query a database concurrently.',
+    author = 'Marco Hemken',
+    author_email = 'mehemken@gmail.com',
+    url = 'https://github.com/mehemken/queruntine',
+    packages = []
+)
